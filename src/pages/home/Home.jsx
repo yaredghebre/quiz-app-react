95aa0bf0fcc35ebabe149e9a50086d99c8a43dbc
yaredghebre <yared2391@gmail.com>
2023-08-05 11:36:22 -0300

    Variables and error working

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
-import { Button, MenuItem, TextField } from "@mui/material";
+import { Alert, Button, MenuItem, TextField } from "@mui/material";
 import Categories from "../../data/Categories";
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
+const Home = ({ name, setName }) => {
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [error, setError] = useState(false);
+
+  const history = useNavigate();
+
+  const handleSubmit = () => {
+    if (!category || !difficulty || !name) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+      fecthQuestions(category, difficulty);
+      history.push("/quiz");
+    }
+  };
+
   return (
     <div className="content">
       <div className="settings">
         <span style={{ fontSize: 30 }}>Quiz Settings</span>
 
         <div className="settings-select">
+          {error && (
+            <Alert style={{ marginBottom: 25 }} severity="error">
+              Please fill out all the fields!
+            </Alert>
+          )}
+
           {/* NAME */}
           <TextField
             style={{ marginBottom: 25 }}
             label="Enter your name"
             variant="outlined"
+            onChange={(e) => setName(e.target.value)}
           />
 
           {/* CATEGORY */}
@@ -23,6 +48,8 @@ const Home = () => {
             select
             label="Select category"
             variant="outlined"
+            onChange={(e) => setCategory(e.target.value)}
+            value={category}
           >
             {Categories.map((item) => (
               <MenuItem key={item.category} value={item.value}>
@@ -37,6 +64,8 @@ const Home = () => {
             style={{ marginBottom: 30 }}
             label="Select Difficulty"
             variant="outlined"
+            onChange={(e) => setDifficulty(e.target.value)}
+            value={difficulty}
           >
             <MenuItem key="Easy" value="easy">
               Easy
@@ -52,7 +81,12 @@ const Home = () => {
           </TextField>
 
           {/* BUTTON */}
-          <Button variant="contained" color="secondary">
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            onClick={handleSubmit}
+          >
             Start Quiz
           </Button>
         </div>
