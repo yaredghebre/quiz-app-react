8e970584ca2705ff71166e0e2b48a77cbdf81c4d
yaredghebre <yared2391@gmail.com>
2023-08-19 09:01:33 -0300

    add function to shuffle answers

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,9 +1,25 @@
 import React, { useEffect } from "react"; //to be called once component it's rendered
 
 const Quiz = ({ name, score, questions, setQuestions, setScore }) => {
+  const [options, setOptions] = useState();
+  const [currQuestion, setCurrQuestion] = useState(0);
+
   useEffect(() => {
     console.log(questions);
+
+    setOptions(
+      questions &&
+        handleShuffle([
+          //to shuffle answers
+          questions[currQuestion]?.correct_answer,
+          ...questions[currQuestion]?.incorrect_answers,
+        ])
+    );
   }, [questions]);
+
+  const handleShuffle = (possibilities) => {
+    return possibilities.sort(() => Math.random() - 0.5); // used function to randomize answers
+  };
 
   return <div>Quiz</div>;
 };
